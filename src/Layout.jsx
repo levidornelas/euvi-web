b777da5715f2d2dbf416cb5f93339d2da94e762a
Levi Dornelas <levidornelas2006@gmail.com>
2025-01-30 11:55:13 -0300

    change background

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -4,17 +4,14 @@ import "./index.css";
 
 export default function RootLayout({ children }) {
   const location = useLocation();
-
-  // Verifica se estamos na página de detalhes
   const isDetailsPage = location.pathname.startsWith("/details");
 
   return (
     <div
-      className={`h-screen w-screen flex items-center justify-center ${isDetailsPage ? "bg-zinc-500" : "bg-zinc-500"
-        }`} // Ajusta o fundo conforme a página
+      className="h-screen w-screen flex items-center justify-center bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: `url(/bg-img.png)` }}
     >
       <div className="relative h-full sm:min-h-[95vh] w-[400px] bg-[#145CCC] shadow-xl overflow-auto">
-        {/* A tela de celular simula uma tela cheia em dispositivos menores e com bordas no desktop */}
         <div className="h-full w-full relative">{children}</div>
       </div>
     </div>
